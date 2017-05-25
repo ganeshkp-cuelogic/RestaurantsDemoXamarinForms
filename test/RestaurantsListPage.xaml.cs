a65d472ba87a26cf9d20c1e16aa2d308df58b181
Ganesh patro <ganesh@Ganesh-Patros-Mac-mini.local>
2017-05-25 13:40:55 +0530

    First Commit

---
+++ test/RestaurantsListPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace test
+{
+	public partial class RestaurantsListPage : ContentPage
+	{
+		public RestaurantsListPage()
+		{
+			InitializeComponent();
+		}
+
+		#region
+		public void onSearchButtonClicked(object sender, EventArgs e)
+		{
+
+		}
+
+		public void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+
+		}
+		#endregion
+
+
+	}
+}
