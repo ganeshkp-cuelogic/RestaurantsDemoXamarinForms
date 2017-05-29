8ffbe5ca4de985cc3b68fb93854c59a480563df0
Ganesh patro <ganesh@Ganesh-Patros-Mac-mini.local>
2017-05-29 11:06:22 +0530

    - Restaurants Search

--- test/RestaurantsListPage.xaml.cs
+++ test/RestaurantsListPage.xaml.cs
@@ -1,29 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+
 
 using Xamarin.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace test
 {
 	public partial class RestaurantsListPage : ContentPage
 	{
+		void Handle_SearchButtonPressed(object sender, System.EventArgs e)
+		{
+			listView.ItemsSource = new List<RestruantModel>();
+			SearchRestaurantsAsync(SearchBarRestaurants.Text, (restaurantsResponse) =>
+			{
+				listView.ItemsSource = restaurantsResponse.restaurants;
+			});
+		}
+
 		public RestaurantsListPage()
 		{
 			InitializeComponent();
+			Title = "Search Restaurants";
 		}
 
-		#region
-		public void onSearchButtonClicked(object sender, EventArgs e)
+		async void SearchRestaurantsAsync(string searchText, Action<RestruantsResponse> callback)
 		{
+			HttpClient client = new HttpClient();
+			string url = "https://developers.zomato.com/api/v2.1/search?q=" + searchText;
+			var uri = new Uri(url);
+			HttpResponseMessage response = null;
+			client.MaxResponseContentBufferSize = 2560000;
+			client.DefaultRequestHeaders.Add("user-key", "216b69fdc8edd8900e3721e83c543591");
+			response = await client.GetAsync(uri);
+			var contentBody = await response.Content.ReadAsStringAsync();
 
+			//Parse the response
+			RestruantsResponse restaurantResponse = JsonConvert.DeserializeObject<RestruantsResponse>(contentBody);
+			List<RestruantModel> restModels = new List<RestruantModel>();
+			Newtonsoft.Json.Linq.JObject obj = Newtonsoft.Json.Linq.JObject.Parse(contentBody);
+			IList<JToken> results = obj["restaurants"].Children().ToList();
+			foreach (JToken result in results)
+			{
+				RestruantModel restModel = result.ToObject<RestruantModel>();
+				restModel.restaurant.address = result["restaurant"]["location"]["address"].ToString();
+				restModels.Add(restModel);
+			}
+			restaurantResponse.restaurants = restModels;
+			callback(restaurantResponse);
 		}
-
-		public void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
-		{
-
-		}
-		#endregion
-
-
 	}
 }
---
+++ test/RestruantModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+	public class RestruantModel
+	{
+		public RestruantInfo restaurant { get; set; }
+
+		public RestruantModel(RestruantInfo info)
+		{
+			this.restaurant = info;
+		}
+	}
+
+	public class RestruantInfo
+	{
+		public string id { get; set; }
+		public string name { get; set; }
+		public string thumb { get; set; }
+		public string cuisines { get; set; }
+		public string average_cost_for_two { get; set; }
+		public string address { get; set; }
+	}
+
+	public class RestruantsResponse
+	{
+		public RestruantsResponse()
+		{
+		}
+		public List<RestruantModel> restaurants { get; set; }
+	}
+}
